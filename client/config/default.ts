962f602a948077667783b00b2db38e05ca9bccdb
chronoshivt <75964136+chronoshivt@users.noreply.github.com>
2023-02-21 16:44:27 -0600

    Update default.ts

--- client/config/default.ts
+++ client/config/default.ts
@@ -1,6 +1,6 @@
 export const SOCKET_URL =
-  // process.env.SOCKET_URL || "https://chatifyserver.onrender.com";
-  process.env.SOCKET_URL || "http://localhost:4000";
+  process.env.SOCKET_URL || "https://chatifyserver.onrender.com";
+  // process.env.SOCKET_URL || "http://localhost:4000";
 
 
   //https://chatifyserver.onrender.com
